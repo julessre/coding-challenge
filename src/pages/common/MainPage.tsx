52d01af4237c62b2a54b59a009f9dc1b7ef7033a
Julia <jules.rechi@gmail.com>
2024-05-02 10:58:41 +0200

    Add some styling

--- src/pages/common/MainPage.tsx
+++ src/pages/common/MainPage.tsx
@@ -8,32 +8,35 @@ export const MainPage = () => {
     const boards = boardQuery.isSuccess ? boardQuery.data : [];
 
     return (
-        <div className={"main-page"}>
+        <div className={"mainPage"}>
             <h2>Main Page</h2>
             {boardQuery.isLoading ? <Loader /> : null}
-            {boards.map((board) => {
-                return (
-                    <div key={board.id}>
-                        {board.name}
-                        <button
-                            onClick={async () => {
-                                //TODO: example to create a hardcoded task
-                                const response = await addTask.mutateAsync({
-                                    boardId: board.id,
-                                    task: {
-                                        name: "test",
-                                        // TODO: status can be omitted
-                                        // status: "in progress"
-                                    },
-                                });
-                                console.log(response);
-                            }}
-                        >
-                            Add Task
-                        </button>
-                    </div>
-                );
-            })}
+            <div className={"boardView"}>
+                {boards.map((board) => {
+                    return (
+                        <div key={board.id} className={"block"}>
+                            {board.name}
+                            <button
+                                className={"buttonAddTask"}
+                                onClick={async () => {
+                                    //TODO: example to create a hardcoded task
+                                    const response = await addTask.mutateAsync({
+                                        boardId: board.id,
+                                        task: {
+                                            name: "test",
+                                            // TODO: status can be omitted
+                                            // status: "in progress"
+                                        },
+                                    });
+                                    console.log(response);
+                                }}
+                            >
+                                Add Task
+                            </button>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 };
