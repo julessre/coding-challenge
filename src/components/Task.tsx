94b1100e533f6f3578e2f024cc5309df5eac8d64
Julia <jules.rechi@gmail.com>
2024-05-03 10:38:09 +0200

    Add other boards

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,14 +1,14 @@
-export type Task = {
-    id: string;
-    name: string;
-    status: "todo" | "in progress" | "done";
-};
+// export type Task = {
+//     id: string;
+//     name: string;
+//     status: "todo" | "in progress" | "done";
+// };
 
-export default function Task(task: Task) {
-    return (
-        <div>
-            <h2>{task.name}</h2>
-            <p>Status: {task.status}</p>
-        </div>
-    );
-}
+// export default function Task(task: Task) {
+//     return (
+//         <div>
+//             <h2>{task.name}</h2>
+//             <p>Status: {task.status}</p>
+//         </div>
+//     );
+// }
--- src/pages/common/MainPage.tsx
+++ src/pages/common/MainPage.tsx
@@ -9,7 +9,10 @@ export const MainPage = () => {
     const boards = boardQuery.isSuccess ? boardQuery.data : [];
     const [newTask, setNewTask] = useState("");
     const [status, setStatus] = useState("todo");
-    const tasksQuery = useGetTasks(boards[1]?.id);
+    const [board1Task, setBoard1Task] = useState("");
+    const [board1Status, setBoard1Status] = useState("todo");
+    const [board2Task, setBoard2Task] = useState("");
+    const [board2Status, setBoard2Status] = useState("todo");
 
     async function handleAddTask(boardId: string) {
         try {
@@ -27,6 +30,53 @@ export const MainPage = () => {
         }
     }
 
+    return (
+        <div className={"mainPage"}>
+            <h2>Main Page</h2>
+            {boardQuery.isLoading ? <Loader /> : null}
+
+            <div className={"boardView"}>
+                {boards.map(
+                    (board) =>
+                        (board.id === "f1568aac-c362-48fc-8cce-cdaeb1c57877" ||
+                            board.id === "b08890b5-c0e9-4e4f-9490-ce7eda059bfd") && (
+                            <div key={board.id} className={"block"}>
+                                <h3>{board.name}</h3>
+                                <div className={"createTask"}>
+                                    <input
+                                        type="text"
+                                        value={newTask}
+                                        onChange={(e) => setNewTask(e.target.value)}
+                                        placeholder="Enter task name"
+                                        className={"inputField"}
+                                    />
+                                    <select
+                                        value={status}
+                                        onChange={(e) => setStatus(e.target.value)}
+                                        className={"taskStatus"}
+                                    >
+                                        <option value="todo">Todo</option>
+                                        <option value="in Progress">In Progress</option>
+                                        <option value="done">Done</option>
+                                    </select>
+                                </div>
+                                <button className={"buttonAddTask"} onClick={() => handleAddTask(board.id)}>
+                                    Add Task
+                                </button>
+                                <div>
+                                    <TasksList boardId={board.id} />
+                                </div>
+                            </div>
+                        ),
+                )}
+            </div>
+        </div>
+    );
+};
+
+const TasksList = ({ boardId }: { boardId: string }) => {
+    const tasksQuery = useGetTasks(boardId);
+
     const handleChangeTaskStatus = async (taskId: string, newStatus: string) => {
         try {
             const updatedTask = await updateTaskStatus(taskId, newStatus);
@@ -36,127 +86,61 @@ export const MainPage = () => {
         }
     };
 
-    return (
-        <div className={"mainPage"}>
-            <h2>Main Page</h2>
-            {boardQuery.isLoading ? <Loader /> : null}
-            {boards.length > 0 && (
-                <div className={"boardView"}>
-                    {boards[0] && (
-                        <div key={boards[1].id} className={"block"}>
-                            <h3>{boards[1].name}</h3>
-                            <div className={"createTask"}>
-                                <input
-                                    type="text"
-                                    value={newTask}
-                                    onChange={(e) => setNewTask(e.target.value)}
-                                    placeholder="Enter task name"
-                                    className={"inputField"}
-                                />
-                                <select
-                                    value={status}
-                                    onChange={(e) => setStatus(e.target.value)}
-                                    className={"taskStatus"}
-                                >
-                                    <option value="to do">Todo</option>
-                                    <option value="in Progress">In Progress</option>
-                                    <option value="done">Done</option>
-                                </select>
-                            </div>
-                            <button className={"buttonAddTask"} onClick={() => handleAddTask(boards[1].id)}>
-                                Add Task
+    if (tasksQuery.isLoading) {
+        return <Loader />;
+    }
+
+    if (tasksQuery.isSuccess) {
+        return (
+            <ul className={"toDoList"}>
+                {tasksQuery.data?.items?.map((task) => (
+                    <li key={task.id} className={"singleTasks"}>
+                        <div>{task.name}</div>
+                        <div className={`status ${task.status}`}>{task.status}</div>
+                        <div>
+                            <button
+                                type="button"
+                                className={"changeStatusButton"}
+                                onClick={() => {
+                                    handleChangeTaskStatus(task.id, "in progress").catch((error) => {
+                                        console.log(error);
+                                    });
+                                    tasksQuery.refetch();
+                                }}
+                            >
+                                Start Task
                             </button>
-                            <div>
-                                {tasksQuery.isSuccess && (
-                                    <ul className={"toDoList"}>
-                                        {tasksQuery.data?.items?.map((task) => (
-                                            <li key={task.id} className={"singleTasks"}>
-                                                <div>{task.name}</div>
-                                                <div className={`status ${task.status}`}>{task.status}</div>
-                                                <div>
-                                                    <button
-                                                        type="button"
-                                                        className={"changeStatusButton"}
-                                                        onClick={() => {
-                                                            handleChangeTaskStatus(task.id, "in progress").catch(
-                                                                (error) => {
-                                                                    console.log(error);
-                                                                },
-                                                            );
-                                                            tasksQuery.refetch();
-                                                        }}
-                                                    >
-                                                        Start Task
-                                                    </button>
-                                                    <button
-                                                        type="button"
-                                                        className={"changeStatusButton"}
-                                                        onClick={() => {
-                                                            handleChangeTaskStatus(task.id, "done").catch((error) => {
-                                                                console.log(error);
-                                                            });
-                                                            tasksQuery.refetch();
-                                                        }}
-                                                    >
-                                                        Complete Task
-                                                    </button>
+                            <button
+                                type="button"
+                                className={"changeStatusButton"}
+                                onClick={() => {
+                                    handleChangeTaskStatus(task.id, "done").catch((error) => {
+                                        console.log(error);
+                                    });
+                                    tasksQuery.refetch();
+                                }}
+                            >
+                                Complete Task
+                            </button>
 
-                                                    <button
-                                                        type="button"
-                                                        className={"removeButton"}
-                                                        onClick={() => {
-                                                            deleteTask(task.id).catch((error) => {
-                                                                console.log(error);
-                                                            });
-                                                            tasksQuery.refetch();
-                                                        }}
-                                                    >
-                                                        Remove
-                                                    </button>
-                                                </div>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                )}
-                            </div>
+                            <button
+                                type="button"
+                                className={"removeButton"}
+                                onClick={() => {
+                                    deleteTask(task.id).catch((error) => {
+                                        console.log(error);
+                                    });
+                                    tasksQuery.refetch();
+                                }}
+                            >
+                                Remove
+                            </button>
                         </div>
-                    )}
-                </div>
-            )}
-        </div>
-    );
-};
+                    </li>
+                ))}
+            </ul>
+        );
+    }
 
-//     return (
-//         <div className={"mainPage"}>
-//             <h2>Main Page</h2>
-//             {boardQuery.isLoading ? <Loader /> : null}
-//             <div className={"boardView"}>
-//                 {boards.map((board) => (
-//                     <div key={board.id} className={"block"}>
-//                         <h3>{board.name}</h3>
-//                         <input
-//                             type="text"
-//                             value={newTask}
-//                             onChange={(e) => setNewTask(e.target.value)}
-//                             placeholder="Enter task name"
-//                         />
-//                         <button className={"buttonAddTask"} onClick={() => handleAddTask(board.id)}>
-//                             Add Task
-//                         </button>
-//                         {tasksQuery.isSuccess && board.id === 1 && (
-//                             <ul>
-//                                 {tasksQuery.data?.items?.map((task) => (
-//                                     <li key={task.id}>
-//                                         {task.name} - {task.status}
-//                                     </li>
-//                                 ))}
-//                             </ul>
-//                         )}
-//                     </div>
-//                 ))}
-//
-//             </div>
-//         </div>
-//     );
-// };
+    return null;
+};
